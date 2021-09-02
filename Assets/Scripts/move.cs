1498a89436969477c648bc163969d815cc59f666
gosaigosai <shubhamgirigosai@gmail.com>
2021-09-03 00:16:33 +0530

    movement and naming covention

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,52 +4,21 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    Vector3 right = new Vector3(0.1f, 0, 0); //Vector in the direction you want to move in.
-    Vector3 left = new Vector3(-0.1f, 0, 0);
-    public float moveSpeed;
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.D))
-        {//Using update to capture the keypress.
-            StartCoroutine(smooth_move_right(right, 1 * moveSpeed)); //Calling the coroutine.
-        }
-        if (Input.GetKey(KeyCode.A))
-        {//Using update to capture the keypress.
-            StartCoroutine(smooth_move_left(left, 1 * moveSpeed)); //Calling the coroutine.
-        }
-    }
+	public float speed = 2.0f;
+	public GameObject character;
 
+	void Update()
+	{
 
-    IEnumerator smooth_move_right(Vector3 direction, float speed)
-    {
-        float startime = Time.time;
-        Vector3 start_pos = transform.position; //Starting position.
-        Vector3 end_pos = transform.position ; //Ending position.
-
-        while (start_pos != end_pos && ((Time.time - startime) * speed) < 1f)
-        {
-            float move = Mathf.Lerp(0, 10f, (Time.time - startime) * speed);
-
-            transform.position += direction * move;
-
-            yield return null;
-        }
-    }
-
-    IEnumerator smooth_move_left(Vector3 direction, float speed)
-    {
-        float startime = Time.time;
-        Vector3 start_pos = transform.position; //Starting position.
-        Vector3 end_pos = transform.position + direction; //Ending position.
-
-        while (start_pos != end_pos && ((Time.time - startime) * speed) < 1f)
-        {
-            float move = Mathf.Lerp(0, 10f, (Time.time - startime) * speed);
-
-            transform.position += direction * move;
-
-            yield return null;
-        }
-    }
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		
+	}
 
 }
