51c411b072a1f14df9a0f60c4045fe9f6c049cd7
anish0910 <batman.wayn09@gmail.com>
2021-09-03 18:57:16 +0530

    Coderman101Stars

--- Assets/Star_Script/InfiniteStarfield.cs
+++ Assets/Star_Script/InfiniteStarfield.cs
@@ -1,64 +1,72 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class InfinteStarfield : MonoBehaviour {
+public class InfiniteStarfield : MonoBehaviour 
+{
+    private Transform tx;
+    private ParticleSystem.Particle[] points; 
 
-
-	private Transform tx;
-	private ParticleSystem.Particle[] points;
-
-	public int starsMax = 100;
-	public float starSize = 1;
-	public float starDistance = 10;
-	public float starClipDistance = 1;
-	private float starDistanceSqr;
-	private float starClipDistanceSqr;
-
-
-	// Use this for initialization
-	void Start () {
-		tx = transform;
-		starDistanceSqr = starDistance * starDistance;
-		starClipDistanceSqr = starClipDistance * starClipDistance;
-	}
-
-
-	private void CreateStars() {
-		points = new ParticleSystem.Particle[starsMax];
-
-		for (int i = 0; i; &lt; starsMax; i++) {
-			points[i].position = Random.insideUnitSphere * starDistance + tx.position;
-			points[i].color = new Color(1,1,1, 1);
-			points[i].size = starSize;
-		}
-	}
-
-
-	// Update is called once per frame
-	void Update () {
-		if ( points == null ) CreateStars();
-
-		for (int i = 0; i &lt; starsMax; i++) {
-
-			if ((points[i].position - tx.position).sqrMagnitude &gt; starDistanceSqr) {
-				points[i].position = Random.insideUnitSphere.normalized * starDistance + tx.position;
-			}
-
-			if ((points[i].position - tx.position).sqrMagnitude &lt;= starClipDistanceSqr) {
-				float percent = (points[i].position - tx.position).sqrMagnitude / starClipDistanceSqr;
-				points[i].color = new Color(1,1,1, percent);
-				points[i].size = percent * starSize;
-			}
-
-
-		}
+    public int starsMax = 100;
+    public float starSize = 1;
+    public float starDistance = 10;
+    public float starClipDistance = 1;
+    private float starDistanceSqr;
+    private float starClipDistanceSqr;
 
 
 
-		// older versions of Unity
-		// particleSystem.SetParticles ( points, points.Length );
-		// Unity 5.4+ and probably some sooner versions
-		GetComponent().SetParticles(points, points.Length);
 
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        tx = transform;
+        starDistanceSqr = starDistance * starDistance;
+        starClipDistanceSqr = starClipDistance * starClipDistance;
+        
+    }
+
+    private void CreateStars () {
+        points = new ParticleSystem.Particle[starsMax];
+
+        for (int i = 0; i < starsMax; i++ ) {
+           points[i].position = Random.insideUnitSphere * starDistance + tx.position;
+           points[i].color = new Color(1,1,1, 1);
+           points[i].size = starSize; 
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ( points == null ) CreateStars();
+
+        for (int i = 0; i < starsMax; i++) {
+
+            if ((points[i].position - tx.position) .sqrMagnitude > starDistance) {
+                points[i].position = Random.insideUnitSphere * starDistance + tx.position;
+            }
+
+            if ((points[i].position - tx.position) .sqrMagnitude <= starClipDistanceSqr) {
+                float percent = (points[i].position - tx.position) .sqrMagnitude / starClipDistanceSqr;
+                points[i].color = new Color(1,1,1, percent);
+                points[i].size = percent * starSize;
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+        
+        GetComponent<ParticleSystem>().SetParticles ( points, points.Length );
+
+        
+    }
 }
